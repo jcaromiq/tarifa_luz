a49645097ebdf02722ad52a2e14c07fa2c47054b
Joaquin Caro <jcaromiq@gmail.com>
2021-06-11 00:09:05 +0200

    add weekend to valle zone

--- src/Zone.test.ts
+++ src/Zone.test.ts
@@ -11,6 +11,17 @@ describe("range hours", () => {
         }
 
     });
+
+    test("should get zone valle at weekend", () => {
+        jest.useFakeTimers("modern");
+        let list: number[] = [0, 10, 18];
+        for (let i of list) {
+            jest.setSystemTime(new Date(2021, 5, 12, i, 0, 0));
+            const current: ZoneEnum = currentZone()
+            expect(current).toBe(ZoneEnum.Valle);
+        }
+
+    });
     test("should get zone llano from 8:00 to 10:00 and 14 to 18 and 22 to 00", () => {
         jest.useFakeTimers("modern");
         let list: number[] = [8, 9, 14, 15, 16, 17, 22, 23];
--- src/Zone.ts
+++ src/Zone.ts
@@ -4,25 +4,28 @@ export enum ZoneEnum {
     Llana = "llana",
 }
 
-   export function current(): ZoneEnum {
-        const currentHour = new Date().getHours();
-        if (currentHour >= 0 && currentHour < 8) {
-            return ZoneEnum.Valle;
-        }
-        if (currentHour >= 8 && currentHour < 10) {
-            return ZoneEnum.Llana;
-        }
-        if (currentHour >= 10 && currentHour < 14) {
-            return ZoneEnum.Punta;
-        }
-        if (currentHour >= 14 && currentHour < 18) {
-            return ZoneEnum.Llana;
-        }
-        if (currentHour >= 18 && currentHour < 22) {
-            return ZoneEnum.Punta;
-        }
-        if (currentHour >= 22 && currentHour <= 23) {
-            return ZoneEnum.Llana;
-        }
+export function current(): ZoneEnum {
+    let date = new Date();
+    const currentHour = date.getHours();
+    const dayOfWeek = date.getDay()
+
+    if (currentHour >= 0 && currentHour < 8 || (dayOfWeek == 0 || dayOfWeek == 6)) {
+        return ZoneEnum.Valle;
+    }
+    if (currentHour >= 8 && currentHour < 10) {
+        return ZoneEnum.Llana;
+    }
+    if (currentHour >= 10 && currentHour < 14) {
         return ZoneEnum.Punta;
     }
+    if (currentHour >= 14 && currentHour < 18) {
+        return ZoneEnum.Llana;
+    }
+    if (currentHour >= 18 && currentHour < 22) {
+        return ZoneEnum.Punta;
+    }
+    if (currentHour >= 22 && currentHour <= 23) {
+        return ZoneEnum.Llana;
+    }
+    return ZoneEnum.Punta;
+}
