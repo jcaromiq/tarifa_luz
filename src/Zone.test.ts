c64d288bba70eb62e00534cf32a4e0f579ff19f9
Joaquin Caro <jcaromiq@gmail.com>
2021-06-15 00:10:05 +0200

    fix tests

--- src/Zone.test.ts
+++ src/Zone.test.ts
@@ -1,4 +1,4 @@
-import {current as currentZone, ZoneEnum} from "./Zone";
+import {zone as currentZone, ZoneEnum} from "./Zone";
 
 describe("range hours", () => {
     test("should get zone valle from 0:00 to 8:00", () => {
@@ -6,7 +6,7 @@ describe("range hours", () => {
         let list: number[] = [0, 1, 2, 3, 4, 5, 6, 7];
         for (let i of list) {
             jest.setSystemTime(new Date(2021, 1, 1, i, 0, 0));
-            const current: ZoneEnum = currentZone()
+            const current: ZoneEnum = currentZone().current;
             expect(current).toBe(ZoneEnum.Valle);
         }
 
@@ -17,7 +17,7 @@ describe("range hours", () => {
         let list: number[] = [0, 10, 18];
         for (let i of list) {
             jest.setSystemTime(new Date(2021, 5, 12, i, 0, 0));
-            const current: ZoneEnum = currentZone()
+            const current: ZoneEnum = currentZone().current;
             expect(current).toBe(ZoneEnum.Valle);
         }
 
@@ -27,7 +27,7 @@ describe("range hours", () => {
         let list: number[] = [8, 9, 14, 15, 16, 17, 22, 23];
         for (let i of list) {
             jest.setSystemTime(new Date(2021, 1, 1, i, 0, 0));
-            const current: ZoneEnum = currentZone()
+            const current: ZoneEnum = currentZone().current;
             expect(current).toBe(ZoneEnum.Llana);
         }
 
@@ -38,7 +38,7 @@ describe("range hours", () => {
         let list: number[] = [10, 11, 12, 13, 18, 19, 20, 21];
         for (let i of list) {
             jest.setSystemTime(new Date(2021, 1, 1, i, 0, 0));
-            const current: ZoneEnum = currentZone()
+            const current: ZoneEnum = currentZone().current;
             expect(current).toBe(ZoneEnum.Punta);
         }
 
