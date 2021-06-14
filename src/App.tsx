c37c032360a2165b9b39e837b7edcf8c5a6559a1
Joaquin Caro <jcaromiq@gmail.com>
2021-06-15 00:08:59 +0200

    add next zone

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import React, {useState, useEffect} from "react";
-import {current as currentZone} from "./Zone";
+import {zone as currentZone} from "./Zone";
 import './App.css';
 
 function App() {
-    const [time, setTime] = useState(currentZone());
+    const [zone, setZone] = useState(currentZone());
 
     useEffect(() => {
-        const interval = setInterval(() => setTime(currentZone()), 1000);
+        const interval = setInterval(() => setZone(currentZone()), 1000);
         return () => {
             clearInterval(interval);
         };
     }, []);
     return (
-        <div className="space-y-5 d-flex flex-column justify-content-center pt-5 text-center">
-            <div><h1>Franja actual</h1></div>
-            <div><p className={"text-uppercase font-weight-bold " + time}>Hora {time}</p></div>
-            <p>Hecho con <a href="https://www.buymeacoffee.com/jcaro" target="_blank" rel="noreferrer" >❤️</a> por <a
+        <div className="container p-4">
+            <div className="card-body">
+                <h2 className="display-2 border-bottom"><strong>Franja actual</strong></h2>
+                <div><p className={"text-uppercase display-4 " + zone.current}>Hora {zone.current}</p></div>
+
+            </div>
+            <div className="card-body">
+                <h2 className="display-3 border-bottom"><strong>Franja siguiente</strong></h2>
+                <p className="display-6">8:00 a 10:00</p>
+                <div><p className={"text-uppercase display-4 " + zone.current}>Hora {zone.current}</p></div>
+
+            </div>
+            <p className="lead p-5">Hecho con <a href="https://www.buymeacoffee.com/jcaro" target="_blank" rel="noreferrer" >❤️</a> por <a
                 href="https://github.com/jcaromiq/" target="_blank" rel="noreferrer" >Joaco</a></p>
         </div>
+
+
+
+
     );
 }
 
--- src/Zone.ts
+++ src/Zone.ts
@@ -3,32 +3,34 @@ export enum ZoneEnum {
     Punta = "punta",
     Llana = "llana",
 }
-
-export function current(): ZoneEnum {
+export interface Zone {
+    current:ZoneEnum,
+}
+export function zone(): Zone {
     let date = new Date();
     const currentHour = date.getHours();
     const dayOfWeek = date.getDay()
 
     if (dayOfWeek === 0 || dayOfWeek === 6) {
-        return ZoneEnum.Valle;
+        return {current : ZoneEnum.Valle}
     }
     if (currentHour >= 0 && currentHour < 8) {
-        return ZoneEnum.Valle;
+        return {current : ZoneEnum.Valle}
     }
     if (currentHour >= 8 && currentHour < 10) {
-        return ZoneEnum.Llana;
+        return {current : ZoneEnum.Llana}
     }
     if (currentHour >= 10 && currentHour < 14) {
-        return ZoneEnum.Punta;
+        return {current : ZoneEnum.Punta}
     }
     if (currentHour >= 14 && currentHour < 18) {
-        return ZoneEnum.Llana;
+        return {current : ZoneEnum.Llana}
     }
     if (currentHour >= 18 && currentHour < 22) {
-        return ZoneEnum.Punta;
+        return {current : ZoneEnum.Punta}
     }
     if (currentHour >= 22 && currentHour <= 23) {
-        return ZoneEnum.Llana;
+        return {current : ZoneEnum.Llana}
     }
-    return ZoneEnum.Punta;
+    return  {current : ZoneEnum.Punta}
 }
