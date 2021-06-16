e0d9c4f2335a6e2c95e4256612ecb47707ab6a5f
Joaquin Caro <jcaromiq@gmail.com>
2021-06-16 23:01:17 +0200

    update style

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,26 @@ function App() {
         };
     }, []);
     let nextZone;
+    let current;
     if (zone.weekend) {
         nextZone = <p className="display-6">Lunes a las {zone.to}</p>
-    } else {
-        nextZone = <p className="display-6">a las {zone.to}</p>
+        current = <p className="display-6"> Todo el día</p>
+        } else {
+        nextZone = <p className="display-6">A las {zone.to}</p>
+        current = <p className="display-6"> De {zone.from} a {zone.to}</p>        
     }
     return (
         <div className="container p-4">
             <div className="card-body">
                 <h2 className="display-2 border-bottom"><strong>Franja actual</strong></h2>
-                <div><p className={"text-uppercase display-4 " + zone.current}>Hora {zone.current}</p></div>
+                {current}
+                <div><p className={"text-uppercase display-4 zone " + zone.current}>Hora {zone.current}</p></div>
 
             </div>
             <div className="card-body">
                 <h2 className="display-3 border-bottom"><strong>Franja siguiente</strong></h2>
                 {nextZone}
-                <div><p className={"text-uppercase display-4 " + zone.next}>Hora {zone.next}</p></div>
+                <div><p className={"text-uppercase display-4 zone " + zone.next}>Hora {zone.next}</p></div>
 
             </div>
             <p className="lead p-5">Hecho con <a href="https://www.buymeacoffee.com/jcaro" target="_blank"
