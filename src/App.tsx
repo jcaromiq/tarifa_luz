1b82e8b45f5e7058b5017adc4f0d5cba6ebfb8d4
Joaquin Caro <jcaromiq@gmail.com>
2021-06-29 00:56:08 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,44 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import {zone as currentZone} from "./Zone";
+import useInterval from "./useInterval";
 import './App.css';
 
 function App() {
     const [zone, setZone] = useState(currentZone());
 
-    useEffect(() => {
-        const interval = setInterval(() => setZone(currentZone()), 60000);
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
+    useInterval(() => {
+        setZone(currentZone())
+    }, 1000)
+
+
     let nextZone;
     let current;
     if (zone.weekend) {
-        nextZone = <p className="display-6">Lunes a las {zone.to}</p>
-        current = <p className="display-6"> Todo el día</p>
+        nextZone = `Lunes a las ${zone.to}`
+        current = 'Todo el día'
     } else {
-        nextZone = <p className="display-6">A las {zone.to}</p>
-        current = <p className="display-6"> De {zone.from} a {zone.to}</p>
+        nextZone = `A las ${zone.to}`
+        current = `De ${zone.from} a ${zone.to}`
     }
     return (
         <div className="container p-4">
             <div className="card-body">
                 <h2 className="display-2 border-bottom"><strong>Franja actual</strong></h2>
-                {current}
+                <p className="display-6">{current}</p>
                 <div><p className={"text-uppercase display-4 zone " + zone.current}>Hora {zone.current}</p></div>
 
             </div>
             <div className="card-body">
                 <h2 className="display-3 border-bottom"><strong>Franja siguiente</strong></h2>
-                {nextZone}
+                <p className="display-6">{nextZone}</p>
                 <div><p className={"text-uppercase display-4 zone " + zone.next}>Hora {zone.next}</p></div>
 
             </div>
-            <p className="lead p-5">Hecho con <a className="text-decoration-none" href="https://www.buymeacoffee.com/jcaro" target="_blank"
+            <p className="lead p-5">Hecho con <a className="text-decoration-none"
+                                                 href="https://www.buymeacoffee.com/jcaro" target="_blank"
                                                  rel="noreferrer">☕️️</a> por <a
-                className="text-decoration-none" href="https://github.com/jcaromiq/tarifa_luz" target="_blank" rel="noreferrer">Joaco</a></p>
+                className="text-decoration-none" href="https://github.com/jcaromiq/tarifa_luz" target="_blank"
+                rel="noreferrer">Joaco</a></p>
         </div>
     );
 }
--- src/Zone.ts
+++ src/Zone.ts
@@ -5,6 +5,7 @@ export enum ZoneEnum {
 }
 
 export interface Zone {
+    id: number,
     current: ZoneEnum,
     weekend: boolean,
     from: number,
@@ -13,20 +14,20 @@ export interface Zone {
 }
 
 export const zones: Zone[] = [
-    {current: ZoneEnum.Valle, weekend: true, from: 0, to: 8, next: ZoneEnum.Punta},
-    {current: ZoneEnum.Valle, weekend: false, from: 0, to: 8, next: ZoneEnum.Llana},
-    {current: ZoneEnum.Llana, weekend: false, from: 8, to: 10, next: ZoneEnum.Punta},
-    {current: ZoneEnum.Punta, weekend: false, from: 10, to: 14, next: ZoneEnum.Llana},
-    {current: ZoneEnum.Llana, weekend: false, from: 14, to: 18, next: ZoneEnum.Punta},
-    {current: ZoneEnum.Punta, weekend: false, from: 18, to: 22, next: ZoneEnum.Llana},
-    {current: ZoneEnum.Llana, weekend: false, from: 22, to: 24, next: ZoneEnum.Valle}]
+    {id:1, current: ZoneEnum.Valle, weekend: true, from: 0, to: 8, next: ZoneEnum.Punta},
+    {id:2, current: ZoneEnum.Valle, weekend: false, from: 0, to: 8, next: ZoneEnum.Llana},
+    {id:3, current: ZoneEnum.Llana, weekend: false, from: 8, to: 10, next: ZoneEnum.Punta},
+    {id:4, current: ZoneEnum.Punta, weekend: false, from: 10, to: 14, next: ZoneEnum.Llana},
+    {id:5, current: ZoneEnum.Llana, weekend: false, from: 14, to: 18, next: ZoneEnum.Punta},
+    {id:6, current: ZoneEnum.Punta, weekend: false, from: 18, to: 22, next: ZoneEnum.Llana},
+    {id:7, current: ZoneEnum.Llana, weekend: false, from: 22, to: 24, next: ZoneEnum.Valle}]
 
 export function zone(): Zone {
     let date = new Date()
     const currentHour = date.getHours()
     const isWeekend = date.getDay() === 0 || date.getDay() === 6
     const inRange = (value: Zone) => currentHour >= value.from && currentHour < value.to;
-    if(isWeekend) {
+    if (isWeekend) {
         return zones
             .find(value => value.weekend)!
     }
