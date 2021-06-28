98eeb7c49edb25ed05b9347f0671c030c0f6a35a
Joaquin Caro <jcaromiq@gmail.com>
2021-06-28 23:38:19 +0200

    refactor

--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -1,27 +1,53 @@
 import {parse} from "https://deno.land/std@0.99.0/datetime/mod.ts";
 import {ensureDirSync} from "https://deno.land/std@0.99.0/fs/mod.ts";
-const TOKEN = Deno.env.get("API_TOKEN");
 
-const response = await fetch("https://api.esios.ree.es/indicators/10391?geo_ids[]=8741", {
-    method: "GET",
-    headers: {
-        "Authorization": `Token token=${TOKEN}`,
-    },
-});
+const TOKEN = Deno.env.get("API_TOKEN")
 
-const jsonData = await response.json();
 
-const updatedAt = jsonData.indicator.values_updated_at;
-const prices = jsonData.indicator.values.map(({value, datetime}: any) => {
-    return {
-        price: (value / 1000).toFixed(4),
-        hour: parse(datetime, "yyyy-MM-ddTHH:mm:ss.SSS'+'02:00").getHours()
-    }
-});
+main()
 
-const file = {
-    prices: prices,
-    updatedAt
+function main() {
+    getPrices()
+        .then(parseData)
+        .then(writeFile)
 }
-ensureDirSync("public/data")
-Deno.writeTextFileSync("public/data/prices.json", JSON.stringify(file))
+
+function getPrices(): Promise<any> {
+    const response = fetch("https://api.esios.ree.es/indicators/10391?geo_ids[]=8741", {
+        method: "GET",
+        headers: {
+            "Authorization": `Token token=${TOKEN}`,
+        },
+    })
+    return response.then(j => j.json())
+}
+
+function parseData(jsonData: any): Prices {
+    const updatedAt = jsonData.indicator.values_updated_at
+    const prices = jsonData.indicator.values.map(({value, datetime}: any) => {
+        return {
+            price: (value / 1000).toFixed(4),
+            hour: parse(datetime, "yyyy-MM-ddTHH:mm:ss.SSS'+'02:00").getHours()
+        }
+    })
+
+    return {
+        prices: prices,
+        updatedAt
+    }
+}
+
+function writeFile(prices: Prices) {
+    ensureDirSync("public/data")
+    Deno.writeTextFileSync("public/data/prices.json", JSON.stringify(prices))
+}
+
+interface Price {
+    hour: number
+    price: number
+}
+
+interface Prices {
+    updatedAt: string
+    prices: Array<Price>
+}
