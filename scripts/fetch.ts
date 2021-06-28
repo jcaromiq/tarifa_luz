e546f0e2f536c74e54c521f4f7bc9d6653cf7cb9
Joaquin Caro <jcaromiq@gmail.com>
2021-06-28 19:44:04 +0200

    format price

--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -2,25 +2,25 @@ import {parse} from "https://deno.land/std@0.99.0/datetime/mod.ts";
 
 const TOKEN = Deno.env.get("API_TOKEN");
 
-const json = fetch("https://api.esios.ree.es/indicators/10391?geo_ids[]=8741", {
+const response = await fetch("https://api.esios.ree.es/indicators/10391?geo_ids[]=8741", {
     method: "GET",
     headers: {
         "Authorization": `Token token=${TOKEN}`,
     },
 });
 
-await json
-    .then((response) => {
-        return response.json();
-    })
-    .then((jsonData) => {
-        return jsonData.indicator.values.map(({value, datetime}: any) => {
-            return {
-                price: value / 1000,
-                hour: parse(datetime, "yyyy-MM-ddTHH:mm:ss.SSS'+'02:00").getHours()
-            }
-        });
-    })
-    .then((json) => {
-        Deno.writeTextFileSync("public/prices.json", JSON.stringify(json));
-    });
+const jsonData = await response.json();
+
+const updatedAt = jsonData.indicator.values_updated_at;
+const prices = jsonData.indicator.values.map(({value, datetime}: any) => {
+    return {
+        price: (value / 1000).toFixed(4),
+        hour: parse(datetime, "yyyy-MM-ddTHH:mm:ss.SSS'+'02:00").getHours()
+    }
+});
+
+const file = {
+    prices: prices,
+    updatedAt
+}
+Deno.writeTextFileSync("public/prices.json", JSON.stringify(file))
