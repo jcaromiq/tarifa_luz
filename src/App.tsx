11a3635944caf226747704f92f4c6ba0f36522fc
Joaquin Caro <jcaromiq@gmail.com>
2021-06-29 11:30:50 +0200

    format code

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,18 @@ function App() {
         <div className="container p-4">
             <div className="card-body">
                 <h2 className="display-2 border-bottom"><strong>Franja actual</strong></h2>
-                <p className="display-6">{zone.weekend? 'Todo el dia': `De ${zone.from} a ${zone.to}` }</p>
-                <div><p className={"text-uppercase display-4 zone " + zone.current}>Hora {zone.current}</p></div>
+                <p className="display-6">{zone.weekend ? 'Todo el dia' : `De ${zone.from} a ${zone.to}`}</p>
+                <div>
+                    <p className={"text-uppercase display-4 zone " + zone.current}>Hora {zone.current}</p>
+                </div>
 
             </div>
             <div className="card-body">
                 <h2 className="display-3 border-bottom"><strong>Franja siguiente</strong></h2>
-                <p className="display-6">{zone.weekend? `Lunes a las ${zone.to}`: `A las ${zone.to}` }</p>
-                <div><p className={"text-uppercase display-4 zone " + zone.next}>Hora {zone.next}</p></div>
+                <p className="display-6">{zone.weekend ? `Lunes a las ${zone.to}` : `A las ${zone.to}`}</p>
+                <div>
+                    <p className={"text-uppercase display-4 zone " + zone.next}>Hora {zone.next}</p>
+                </div>
 
             </div>
             <p className="lead p-5">Hecho con <a className="text-decoration-none"
