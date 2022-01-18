6471317bd43bcd51df273bfa5ae07b9788f6d06b
Joaquin Caro <jcaromiq@gmail.com>
2022-01-18 23:50:45 +0100

    line chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,7 +1,7 @@
 import {
-  Area,
+  Line,
   CartesianGrid,
-  AreaChart,
+  LineChart,
   Tooltip,
   XAxis,
   YAxis,
@@ -12,28 +12,41 @@ import { prices } from "domain/PriceRepository";
 import moment from "moment";
 
 function Chart() {
-  const formatXAxis = (tickItem: any, index: number): string =>
-    moment(tickItem, "YYYY-MM-DD[T]HH:00:00.000Z").hour().toString();
 
   const formatYAxis = (tickItem: any, index: number): string => tickItem + "€";
+
+  const CustomTooltip = ( { active, payload, label} :any):any => {
+    if (active && payload && payload.length) {
+      const day = moment(label, "YYYY-MM-DD[T]HH:00:00.000Z").format("MMM DD, H[h]");
+      const price = payload[0].value;
+      return (
+        <div className="bg-white p-2 border-2 border-bl">
+          <p className="font-bold">{day}</p>
+          <p>{`${price}€`}</p>
+        </div>
+      );
+    }
+    return <></>;
+    
+  };
 
   const { data, isLoading } = useQuery("pricesData", prices);
   if (!isLoading) {
     return (
       <div className="min-w-full h-52 pt-12 pl-2 pr-12">
         <ResponsiveContainer width="100%" height="100%">
-          <AreaChart data={data.prices} width={500} height={200}>
-            <XAxis dataKey="datetime" tickFormatter={formatXAxis} />
+          <LineChart data={data.prices} width={500} height={200}>
+            <XAxis dataKey="datetime" label={"Hora"} tick={false}/>
             <YAxis tickFormatter={formatYAxis} />
-            <Tooltip />
+            <Tooltip content={<CustomTooltip/>}  />
             <CartesianGrid strokeDasharray="3 3" />
-            <Area
+            <Line
               type="linear"
               dataKey="price"
               stroke="#8884d8"
               fill="#8884d8"
             />
-          </AreaChart>
+          </LineChart>
         </ResponsiveContainer>
       </div>
     );
