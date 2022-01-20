76f3daa30776ef7108b2d20526dad517414d61d3
Joaquin Caro <jcaromiq@gmail.com>
2022-01-20 23:05:36 +0100

    style backgound bar color by zone

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,23 +1,27 @@
 import {
-  Line,
   CartesianGrid,
-  LineChart,
+  BarChart,
   Tooltip,
   XAxis,
   YAxis,
   ResponsiveContainer,
+  Bar,
+  Cell,
 } from "recharts";
 import { useQuery } from "react-query";
 import { prices } from "domain/PriceRepository";
 import moment from "moment";
+import { currentZone } from "domain/ZoneRepository";
 
 function Chart() {
-
   const formatYAxis = (tickItem: any, index: number): string => tickItem + "€";
 
-  const CustomTooltip = ( { active, payload, label} :any):any => {
+  const CustomTooltip = ({ active, payload, label }: any): any => {
     if (active && payload && payload.length) {
-      const day = moment(label, "YYYY-MM-DD[T]HH:00:00.000Z").format("MMM DD, H[h]");
+      const day = moment(label, "YYYY-MM-DD[T]HH:00:00.000Z").format(
+        "MMM DD, H[h]"
+      );
+
       const price = payload[0].value;
       return (
         <div className="bg-white p-2 border-2 border-bl">
@@ -27,7 +31,19 @@ function Chart() {
       );
     }
     return <></>;
-    
+  };
+  const bgColor = (item: any): string => {
+    const zone = currentZone(moment(item.datetime).toDate());
+    switch (zone.current) {
+      case "valle":
+        return "#28b62c";
+      case "llana":
+        return "#fff3cd";
+      case "punta":
+        return "#f8d7da";
+      default:
+        return "";
+    }
   };
 
   const { data, isLoading } = useQuery("pricesData", prices);
@@ -35,18 +51,17 @@ function Chart() {
     return (
       <div className="min-w-full h-52 pt-12 pl-2 pr-12">
         <ResponsiveContainer width="100%" height="100%">
-          <LineChart data={data.prices} width={500} height={200}>
-            <XAxis dataKey="datetime" label={"Hora"} tick={false}/>
+          <BarChart data={data.prices} width={500} height={200}>
+            <XAxis dataKey="datetime" label={"Hora"} tick={false} />
             <YAxis tickFormatter={formatYAxis} />
-            <Tooltip content={<CustomTooltip/>}  />
+            <Tooltip content={<CustomTooltip />} />
             <CartesianGrid strokeDasharray="3 3" />
-            <Line
-              type="linear"
-              dataKey="price"
-              stroke="#8884d8"
-              fill="#8884d8"
-            />
-          </LineChart>
+            <Bar dataKey="price" stroke="#8884d8">
+              {data.prices.map((entry: any, index: number) => (
+                <Cell key={`cell-${index}`} fill={bgColor(entry)} />
+              ))}
+            </Bar>
+          </BarChart>
         </ResponsiveContainer>
       </div>
     );
